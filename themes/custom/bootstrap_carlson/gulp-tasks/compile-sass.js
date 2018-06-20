14591eab640c022cdcb5ad44c35c9cd6dcfd57f7
Renata France <renata@creedinteractive.com>
2018-06-20 16:48:13 -0500

    Added .gitignore and feeds, group modules and new bootstrap child theme.

---
+++ themes/custom/bootstrap_carlson/gulp-tasks/compile-sass.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * Task: Compile: Sass.
+ */
+
+module.exports = function (gulp, plugins, options) {
+  'use strict';
+
+  gulp.task('compile:sass', function () {
+    return gulp.src([
+      options.sass.files
+    ])
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.sassGlob())
+      .pipe(plugins.sass({
+        errLogToConsole: true,
+        outputStyle: 'expanded'
+      }))
+      .pipe(plugins.autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(gulp.dest(options.sass.destination));
+  });
+};
