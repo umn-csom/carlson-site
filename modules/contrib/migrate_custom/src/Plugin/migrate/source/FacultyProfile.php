6a9b2e188f965301797afbedfc7c99b9dec239c3
Tony Birleffi <tony@creedinteractive.com>
2018-09-06 00:32:48 -0500

    Just need to port over taxonomy (entity reference) and paragraph (entity reference revisions) data for faculty.

--- modules/contrib/migrate_custom/src/Plugin/migrate/source/FacultyProfile.php
+++ modules/contrib/migrate_custom/src/Plugin/migrate/source/FacultyProfile.php
@@ -25,15 +25,29 @@ class FacultyProfile extends SqlBase {
    * {@inheritdoc}
    */
   public function query() {
+
+    // If it's required use 'join', if not use 'leftjoin'.
     $query = $this->select('node', 'f');
-    $query->join('field_data_field_first_name', 'n', 'n.entity_id = f.nid');
-    $query->join('field_data_field_last_name', 'a', 'a.entity_id = f.nid');
 
+    // Selections.
+    $query->leftjoin('field_data_field_first_name', 'n', 'n.entity_id = f.nid');
+    $query->leftjoin('field_data_field_last_name', 'a', 'a.entity_id = f.nid');
+    $query->leftjoin('field_data_field_staff_title', 'b', 'b.entity_id = f.nid');
+    $query->leftjoin('field_data_field_office_phone', 'j', 'j.entity_id = f.nid');
+    $query->leftjoin('field_data_field_name_middle_initial', 'c', 'c.entity_id = f.nid');
+    $query->leftjoin('field_data_field_address_1', 'd', 'd.entity_id = f.nid');
+    $query->leftjoin('field_data_field_about_me', 'e', 'e.entity_id = f.nid');
+    $query->leftjoin('field_data_field_faculty_profile_owner', 'g', 'g.entity_id = f.nid');
+    $query->leftjoin('field_data_field_department', 'h', 'h.entity_id = f.nid');
+    $query->leftjoin('field_data_field_faculty_email', 'i', 'i.entity_id = f.nid');
+    $query->leftjoin('field_data_field_personal_url', 'k', 'k.entity_id = f.nid');
+    $query->leftjoin('field_data_field_faculty_job_candidate', 'l', 'l.entity_id = f.nid');
+
+
+    // Field Mappings.
     $query->fields('f', array_keys( $this->baseFields() ) );
-    $query->fields('n', array('entity_id', 'field_first_name_value'));
-    $query->fields('a', array('entity_id', 'field_last_name_value'));
 
-    $query->condition('f.nid', 0, '>');
+    $query->condition('f.type', 'faculty');
     return $query;
   }
 
@@ -44,6 +58,35 @@ class FacultyProfile extends SqlBase {
     $fields = $this->baseFields();
     $fields['first_name'] = $this->t('first_name');
     $fields['last_name'] = $this->t('last_name');
+    $fields['office_phone'] = $this->t('office_phone');
+    
+    $fields['staff_title'] = $this->t('staff_title');
+    $fields['profile_title'] = $this->t('profile_title');
+    
+    $fields['name_middle_initial'] = $this->t('name_middle_initial');
+    $fields['middle_initial'] = $this->t('middle_initial');
+    
+    $fields['address_1'] = $this->t('address_1');
+    $fields['profile_address'] = $this->t('profile_address');
+
+    $fields['about_me'] = $this->t('about_me');
+    $fields['body'] = $this->t('body');
+
+    $fields['faculty_profile_owner'] = $this->t('faculty_profile_owner');
+    $fields['profile_owner'] = $this->t('profile_owner');
+
+    $fields['department'] = $this->t('department');
+    $fields['profile_dept'] = $this->t('profile_dept');
+
+    $fields['faculty_email'] = $this->t('faculty_email');
+    $fields['email'] = $this->t('email');
+
+    $fields['personal_url'] = $this->t('personal_url');
+    $fields['profile_url'] = $this->t('profile_url');
+
+    $fields['faculty_job_candidate'] = $this->t('faculty_job_candidate');
+    $fields['profile_job_candidate'] = $this->t('profile_job_candidate');
+
     return $fields;
   }
 
@@ -68,6 +111,77 @@ class FacultyProfile extends SqlBase {
     $result = $this->_getCustomField( 'last_name', $nid );
     foreach ($result as $record) {
       $row->setSourceProperty('last_name', $record->field_last_name_value );
+    }
+
+    // office_phone
+    $result = $this->_getCustomField( 'office_phone', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('office_phone', $record->field_office_phone_value );
+    }
+
+    // staff_title to profile_title
+    $result = $this->_getCustomField( 'staff_title', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('staff_title', $record->field_staff_title_value );
+      $row->setSourceProperty('profile_title', $record->field_staff_title_value );
+    }
+
+    // name_middle_initial to middle_initial
+    $result = $this->_getCustomField( 'name_middle_initial', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('name_middle_initial', $record->field_name_middle_initial_value );
+      $row->setSourceProperty('middle_initial', $record->field_name_middle_initial_value );
+    }
+
+    // address_1 to profile_address
+    $result = $this->_getCustomField( 'address_1', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('address_1', $record->field_address_1_value );
+      $row->setSourceProperty('profile_address', $record->field_address_1_value );
+    }
+
+    // about_me to body
+    $result = $this->_getCustomField( 'about_me', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('about_me', $record->field_about_me_value );
+      $row->setSourceProperty('body/0/value', $record->field_about_me_value );
+    }
+
+    // faculty_profile_owner to profile_owner
+    $result = $this->_getEntityReference( 'faculty_profile_owner', $nid );
+    foreach ($result as $record) {
+      $users = $this->_getUsername( $record->field_faculty_profile_owner_target_id );
+      foreach ($users as $user) {
+        $row->setSourceProperty('faculty_profile_owner', $user->name );
+      }
+    }
+
+    // department to profile_dept
+    $result = $this->_getCustomField( 'department', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('department', $record->field_department_value );
+      $row->setSourceProperty('profile_dept', $record->field_department_value );
+    }
+
+    // faculty_email to email
+    $result = $this->_getEmailField( 'faculty_email', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('faculty_email', $record->field_faculty_email_email );
+      $row->setSourceProperty('email', $record->field_department_value );
+    }
+
+    // personal_url to profile_url
+    $result = $this->_getUrlField( 'personal_url', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('personal_url', $record->field_personal_url_url );
+      $row->setSourceProperty('profile_url', $record->field_personal_url_url );
+    }
+
+    // faculty_job_candidate to profile_job_candidate
+    $result = $this->_getCustomField( 'faculty_job_candidate', $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('faculty_job_candidate', $record->field_faculty_job_candidate_value );
+      $row->setSourceProperty('profile_job_candidate', $record->field_faculty_job_candidate_value );
     }
 
     return parent::prepareRow($row);
@@ -101,6 +215,7 @@ class FacultyProfile extends SqlBase {
       'promote' => $this->t('promote'),
       'sticky' => $this->t('sticky'),
       'uid' => $this->t('uid'),
+      'type' => $this->t('type'),
     );
     return $fields;
 
@@ -121,12 +236,64 @@ class FacultyProfile extends SqlBase {
   }
 
   /**
-   * Private Methods
+   * Private Methods.
    */
   private function _getCustomField($value, $nid) {
     $result = $this->getDatabase()->query('
       SELECT
         fld.field_' . $value . '_value
+      FROM
+        {field_data_field_' . $value . '} fld
+      WHERE
+        fld.entity_id = :nid
+    ', array(':nid' => $nid));
+
+    return $result;
+  }
+
+  private function _getUrlField($value, $nid) {
+    $result = $this->getDatabase()->query('
+      SELECT
+        fld.field_' . $value . '_url
+      FROM
+        {field_data_field_' . $value . '} fld
+      WHERE
+        fld.entity_id = :nid
+    ', array(':nid' => $nid));
+
+    return $result;
+  }
+
+  private function _getEntityReference($value, $nid) {
+    $result = $this->getDatabase()->query('
+      SELECT
+        fld.field_' . $value . '_target_id
+      FROM
+        {field_data_field_' . $value . '} fld
+      WHERE
+        fld.entity_id = :nid
+    ', array(':nid' => $nid));
+
+    return $result;
+  }
+
+  private function _getUsername($uid) {
+    $result = $this->getDatabase()->query('
+      SELECT
+        fld.name
+      FROM
+        users fld
+      WHERE
+        fld.uid = :uid
+    ', array(':uid' => $uid));
+
+    return $result;
+  }
+
+  private function _getEmailField($value, $nid) {
+    $result = $this->getDatabase()->query('
+      SELECT
+        fld.field_' . $value . '_email
       FROM
         {field_data_field_' . $value . '} fld
       WHERE
