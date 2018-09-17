d974bc4a2cd7cd46537b10f20ef8defad0efab33
Tony Birleffi <tony@creedinteractive.com>
2018-09-17 17:55:21 -0500

    Updates.

--- modules/contrib/migrate_custom/src/Plugin/migrate/source/EducationAbroadProgram.php
+++ modules/contrib/migrate_custom/src/Plugin/migrate/source/EducationAbroadProgram.php
@@ -27,6 +27,9 @@ class EducationAbroadProgram extends SqlBase {
     // If it's required use 'join', if not use 'leftjoin'.
     $query = $this->select('node', 'f');
 
+    // Selections.
+    $query->leftjoin('metatag', 'p', 'p.entity_id = f.nid');
+
     // Field Mappings.
     $query->fields('f', array_keys( $this->baseFields() ) );
 
@@ -39,6 +42,8 @@ class EducationAbroadProgram extends SqlBase {
    */
   public function fields() {
     $fields = $this->baseFields();
+    $fields['data'] = $this->t('data');
+    $fields['metatag'] = $this->t('metatag');
     return $fields;
   }
 
@@ -57,6 +62,13 @@ class EducationAbroadProgram extends SqlBase {
     $alias = $this->_setAliasPath( $nid );
     if ( !empty($alias) ) {
       $row->setSourceProperty('alias', '/' . $alias);
+    }
+
+    // metatag
+    $result = $this->_getMetaTags( $nid );
+    foreach ($result as $record) {
+      $row->setSourceProperty('data', $record->data );
+      $row->setSourceProperty('metatag', $record->data );
     }
 
     return parent::prepareRow($row);
@@ -117,6 +129,18 @@ class EducationAbroadProgram extends SqlBase {
     $query = $this->select('url_alias', 'ua')->fields('ua', ['alias']);
     $query->condition('ua.source', 'node/' . $nid);
     return $query->execute()->fetchField();
+  }
+
+  private function _getMetaTags($nid) {
+    $result = $this->getDatabase()->query('
+      SELECT
+        fld.data
+      FROM
+        metatag fld
+      WHERE
+        fld.entity_id = :nid
+    ', array(':nid' => $nid));
+    return $result;
   }
 }
 ?>
