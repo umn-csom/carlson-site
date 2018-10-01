e35282de0faa519517f8c2197c186cbf26728988
joe stephens <joe@creedinteractive.com>
2018-10-01 10:48:15 -0500

    Typography and some small additions

--- themes/custom/barrio_carlson/gulpfile.js
+++ themes/custom/barrio_carlson/gulpfile.js
@@ -116,7 +116,7 @@ gulp.task('js', function() {
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        proxy: "http://localhost:32770/sites/default/themes/custom/barrio_carlson/styleguide/",
+        proxy: "http://carlsonschool8.lndo.site:8000/sites/default/themes/custom/barrio_carlson/styleguide/",
     });
 
     gulp.start('watch');
