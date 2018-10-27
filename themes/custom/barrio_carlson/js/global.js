d80d78d193d63c7a66be06f7252482a6e56dcc55
Renata France <renata@creedinteractive.com>
2018-10-27 12:03:31 -0500

    sticky sidebar

--- themes/custom/barrio_carlson/js/global.js
+++ themes/custom/barrio_carlson/js/global.js
@@ -8,40 +8,44 @@
 
   Drupal.behaviors.bootstrap_barrio_subtheme = {
     attach: function (context, settings) {
+      // run test on initial page load
+      checkStickySize();
 
-      // $('.page-node-1 .carousel').wrap('<div class="col-12 col-md-10 col-lg-8" />');
+      // run test on resize of the window
+      $(window).resize(checkStickySize);
 
-      // var classes = ['random1','random2', 'random3']; //add as many classes as u want
-      // var randomnumber = Math.floor(Math.random()*classes.length);
-      // $('body').addClass(classes[randomnumber]);
+      //Function to the css rule
+      function checkStickySize(){
+          if ($(".sticky-sidebar").css("max-width") == "25%" ){
+            // Check the initial Position of the fixed_nav_container
+            var stickyHeaderTop = $('.sticky-sidebar__inner').offset().top;
+            var wrap_width = $('.landing-page__content--inner').width();
+            var sidebar_width = (wrap_width * .25) - 25;
 
-      // $('.path-galerias #block-sass-starterkit-content').append("<div id='blueimp-gallery' class='blueimp-gallery'><div class='slides' /><h3 class='title' /><a class='prev'>‹</a><a class='next'>›</a><a class='close'>×</a><a class='play-pause' /><ol class='indicator' /></div>");
-      // $('.view-galerias').attr('id', 'links');
+            $(window).scroll(function(){
+              if( $(window).scrollTop() > stickyHeaderTop-109 ) {
+                $('.sticky-sidebar__inner').css({position: 'fixed', top: '109px',width: sidebar_width});  
+              } else {
+                $('.sticky-sidebar__inner').css({position: 'relative', top: '0px',width: 'inherit'});
+              }
+            });
 
-      // $('.triple .view-content').addClass('ml-sm-1 mr-sm-1');
-
-      // document.getElementById('links').onclick = function (event) {
-      //     event = event || window.event;
-      //     var target = event.target || event.srcElement,
-      //         link = target.src ? target.parentNode : target,
-      //         options = {index: link, event: event},
-      //         links = this.getElementsByTagName('a');
-      //     blueimp.Gallery(links, options);
-      // };
-
-
-
+            $(window).on('scroll', function () {
+              if ($(window).scrollTop() >= 10) {
+                $('.carlson-header, .umnhf-campus-tc').addClass('compressed');
+              } else {
+                $('.carlson-header, .umnhf-campus-tc').removeClass('compressed');
+              }
+            });
+          }else {
+            $('.sticky-sidebar__inner').css({position: 'relative', top: '0px',width: 'inherit'});
           }
-        };
+      }
 
-        $(window).on('scroll', function () {
-          if ($(window).scrollTop() >= 10) {
-                  $('.carlson-header, .umnhf-campus-tc').addClass('compressed');
-        } else {
-            $('.carlson-header, .umnhf-campus-tc').removeClass('compressed');
-        }
-    });
 
-    document.querySelector(".card-flip").classList.toggle("flip");
 
+      document.querySelector(".card-flip").classList.toggle("flip");
+
+    }
+  }
 })(jQuery, Drupal);
