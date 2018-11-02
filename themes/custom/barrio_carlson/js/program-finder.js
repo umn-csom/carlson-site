66d15c2eee12c19f588276b287585b279a06c5be
Jana <jana@creedinteractive.com>
2018-11-02 11:38:12 -0500

    fill out the program finder config, adjust a few more styles to make it useful

--- themes/custom/barrio_carlson/js/program-finder.js
+++ themes/custom/barrio_carlson/js/program-finder.js
@@ -2,6 +2,12 @@
  * @file
  * Homepage Program Finder
  *
+ * configuration object defines how the finder functions
+ * - object path should be unique; format is a hierarchy definition (i.e. showing grandparent/parent/child path)
+ * on individual object choice,
+ * - a goto property defines a page that will be available to go to
+ * - any actions property defines a list of dropdown options that will be added to the finder
+ * - goto will be executed first, and if absent, the actions property logic will be executed
  */
 (function ($, Drupal) {
     'use strict';
@@ -15,8 +21,7 @@
                     "path": "2", "label": "Non-Degree",
                     "actions": [
                         { "path": "2/3", "label": "Undergrad", "goto": "/node/95816" }
-                        , {
-                            "path": "2/4", "label": "MBA",
+                        , { "path": "2/4", "label": "MBA",
                             "actions": [
                                 { "path": "2/4/5", "label": "Full-Time MBA", "goto": "/node/95816" }
                                 , { "path": "2/4/6", "label": "Part-Time MBA", "goto": "/node/95816" }
@@ -27,49 +32,86 @@
                                 , { "path": "2/4/11", "label": "Dual Degree", "goto": "/node/95816" }
                             ]
                         }
+                        , { "path": "2/12", "label": "Specialty Masters",
+                            "actions": [
+                                { "path": "2/12/13", "label": "Accountancy", "goto": "/node/95816" }
+                                , { "path": "2/12/14", "label": "Business Analytics", "goto": "/node/95816" }
+                                , { "path": "2/12/15", "label": "Supply Chain Management", "goto": "/node/95816" }
+                                , { "path": "2/12/16", "label": "Business Taxation", "goto": "/node/95816" }
+                                , { "path": "2/12/17", "label": "Human Resources and Industrial Relation", "goto": "/node/95816" }
+                            ]
+                        }
+                        , { "path": "2/18", "label": "Phd",
+                            "actions": [
+                                { "path": "2/18/19", "label": "Phd-Business Administration", "goto": "/node/95816" }
+                                , { "path": "2/18/20", "label": "Global DBA-China", "goto": "/node/95816" }
+                            ]
+                        }
                     ]
                 }
             ]
         };
 
         var _bind = function () {
-
             if ($('.program-finder').length > 0) {
                 var template = _.template($('#carlson-tpl-program-finder-dropdown').html());
-
                 $('.program-finder__choices').html(template(programFinder.config));
-
-                $('.program-finder__choice').each(function () {
-                    $(this).find('.program-finder__choice--option-label').first().addClass('active');
-                });
             }
-
         };
 
         var _choose = function (elem) {
-            $('.program-finder__choice--option-label').removeClass('active');
+            var $dropdown = $(elem).closest('.program-finder__choice');
+            $dropdown.find('.program-finder__choice--option-label').removeClass('active');
             var $chosen = $(elem);
             $chosen.addClass('active');
-            var $dropdown = $(elem).closest('.program-finder__choice');
             $dropdown.toggleClass('open');
             if (!$dropdown.hasClass('open')) {
-                $dropdown.next('.program-finder_choice').remove();
-                var selectedVal = $(elem).closest('.program-finder__choice--option').val() + "";
-                console.log('selected value', selectedVal);
-                //if the value is a path, let's decide if a new dropdown or a goto link is needed
+                $dropdown.parent().nextAll().remove();
+                var selectedVal = $(elem).closest('.program-finder__choice--option').attr('data-value');
                 if (selectedVal != 0) {
-                    console.log('do something');
-                    //find the node we're looking at
-                    //if goto, set link and activate the button
-                    //if actions, add a dropdown to the page with the options
+                    _.each(programFinder.config.actions, function (action) { programFinder.searchActionsFor(action, selectedVal); }, selectedVal);
+                } else {
+                    $('#program-finder__result-go').attr('data-goto', '').attr('disabled', '');
                 }
+            }
+        };
+
+        var _searchActionsFor = function (action, path) {
+            if (action.path == path) {
+                programFinder.performActionFor(action);
+            }
+            if (action.hasOwnProperty('actions')) {
+                _.each(action.actions, function (action) { programFinder.searchActionsFor(action, path); }, path)
+            }
+        };
+
+        var _performActionFor = function (action) {
+            console.log('found it!', action);
+            if (action.hasOwnProperty('goto')) {
+                $('#program-finder__result-go').attr('data-goto', action.goto).removeAttr('disabled');
+            } else {
+                $('#program-finder__result-go').attr('data-goto', '').attr('disabled', '');
+                if (action.hasOwnProperty('actions')) {
+                    var template = _.template($('#carlson-tpl-program-finder-dropdown').html());
+                    $('.program-finder__choices').append(template(action));
+                }
+            }
+        };
+
+        var _go = function (elem) {
+            var $goto = $(elem).attr('data-goto');
+            if ($goto != undefined && $goto != '') {
+                window.location = $goto;
             }
         };
 
         return {
             config: _config,
             bind: _bind,
-            choose: _choose
+            choose: _choose,
+            searchActionsFor: _searchActionsFor,
+            performActionFor: _performActionFor,
+            go: _go
         }
     }());
 
