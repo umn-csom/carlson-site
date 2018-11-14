01fe6cfc8843d6f73394f44887c8d16d07567a1f
Tony Birleffi <tony@creedinteractive.com>
2018-11-14 13:51:21 -0600

    Finished up the sidebar nav styles, just need to make it sticky next.

--- themes/custom/barrio_carlson/js/page-navs.js
+++ themes/custom/barrio_carlson/js/page-navs.js
@@ -8,10 +8,17 @@
 
   $(document).on('click', '.sticky-menu__label', function(e) {
     e.preventDefault();
-    console.log('here');
     var $this = $(this);
     $this.toggleClass('sticky-menu__label--opened');
     $('.sticky-menu__nav').toggleClass('sticky-menu__nav--closed');
   });
+
+  $(document).on('click', '.sticky-menu__item--has-submenu', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    $this.toggleClass('sticky-menu__item--opened');
+    $this.find('ul.subnav').toggleClass('subnav--opened');
+  });
+
   
   })(jQuery, Drupal);  
