f2850fb368d3d836bf884cf17eb0c5d6bd29c04c
Tony Birleffi <tony@creedinteractive.com>
2018-11-20 16:38:01 -0600

    Fixed more of the sticky menu stuff.

--- themes/custom/barrio_carlson/js/page-navs.js
+++ themes/custom/barrio_carlson/js/page-navs.js
@@ -16,14 +16,29 @@
       // run test on resize of the window
       $(window).resize(checkStickySize);
       $(window).resize(checkListeners);
+      $('.sticky-menu').on('sticky-start', stickyStart);
+      $('.sticky-menu').on('sticky-end', stickyEnd);
+
+      function stickyStart() {
+        var $wh = $(window).height();
+        if( $wh < $('.sticky-menu').height() ) {
+          $('.sticky-menu').addClass('freeze');
+        }
+      }
+
+      function stickyEnd() {
+        $('.sticky-menu').removeClass('freeze');
+      }
 
       //Function to the css rule
       function checkStickySize() {
         var wrap_width = $('html').width();
+        $('.sticky-menu').removeClass('freeze');
+
         if (wrap_width >= 992) {
           $('.sticky-menu').sticky({
             topSpacing: 90,
-            bottomSpacing: 470
+            bottomSpacing: 800
           });
         } else {
           $('.sticky-menu').unstick();
