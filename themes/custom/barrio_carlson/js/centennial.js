afd06135508dcff42970aede9a3add4f934d7c8c
Jana <jana@creedinteractive.com>
2019-01-11 08:42:22 -0600

    add mobile behavior for the timeline nav

--- themes/custom/barrio_carlson/js/centennial.js
+++ themes/custom/barrio_carlson/js/centennial.js
@@ -31,6 +31,7 @@
       function highlightTimeline() {
         var found = false;
         var highlightId = null;
+        var isDesktop = (($(window).width() >= 768) ? true : false);
         $.each($('.centennial__timeline--story-group-wrapper'), function () {
           if (!found) {
             var distance = $(this).offset().top - $(window).scrollTop();
@@ -44,13 +45,28 @@
         if (highlightId != null) {
           var $navLi = $('.centennial__timeline--nav-item[data-nav-ref="' + highlightId + '"]');
           var $navUl = $('.centennial__timeline--nav');
-          var currentScroll = $navUl.scrollLeft();
-          var goToScroll = $navLi.position().left - $('.centennial__timeline--nav-item').first().position().left;
+          var currentScroll = 0;
+          var goToScroll = 0;
+          if (isDesktop) {
+            currentScroll = $navUl.scrollLeft();
+            goToScroll = $navLi.position().left - $('.centennial__timeline--nav-item').first().position().left;
+          } else {
+            currentScroll = $navUl.scrollTop();
+            goToScroll = $navLi.position().top - $('.centennial__timeline--nav-item').first().position().top;
+            console.log(currentScroll, 'to', goToScroll);
+          }
           var $anchor = $navLi.find('a');
           if (!$anchor.hasClass('active-anchor')) {
-            $navUl.animate({
-              scrollLeft: goToScroll
-            });
+            if (isDesktop) {
+              $navUl.animate({
+                scrollLeft: goToScroll
+              });
+            } else {
+              $navUl.animate({
+                scrollTop: goToScroll
+              });
+              console.log('scrolled to ' + $navUl.scrollTop(), 'meant to go to ' + goToScroll);
+            }
             $('.centennial__timeline--nav-item-anchor').removeClass('active-anchor');
             $anchor.addClass('active-anchor');
           }
@@ -58,6 +74,24 @@
           $('.centennial__timeline--nav-item-anchor').first().addClass('active-anchor');
         }
       }
+
+      $('.centennial__timeline--nav-item-anchor').click(function (event) {
+        event.preventDefault();
+        var isDesktop = (($(window).width() >= 768) ? true : false);
+        if (!isDesktop && !$('.centennial__timeline--nav-container').hasClass('choose-year')) {
+          $('.centennial__timeline--nav-container').addClass('choose-year');
+        } else {
+          $('.centennial__timeline--nav-container').removeClass('choose-year');
+          if ($('.carlson-header').is(':visible')) {
+            $('.carlson-header').hide();
+          }
+          $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+          }, 1000, function () {
+            $(this.hash).focus();
+          });
+        }
+      });
     }
   }
 
