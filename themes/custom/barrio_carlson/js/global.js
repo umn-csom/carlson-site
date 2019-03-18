45960492054d7874fbf63185e7edda4e0b78971b
Renata France <renata@creedinteractive.com>
2019-03-18 13:27:31 -0500

    icon updates, templates and search styles for cards

--- themes/custom/barrio_carlson/js/global.js
+++ themes/custom/barrio_carlson/js/global.js
@@ -38,9 +38,9 @@
       // Add chosen select to input lists on hub
       var config = {
         // custom field created in lawseq module
-        '.news-hub-search #edit-field-hub-topic-target-id' : {placeholder_text_multiple: "Select Topic(s)"},
-        '.news-hub-search #edit-type-1' : {placeholder_text_multiple: "Select Type(s)"},
-        '.news-hub-search #edit-field-hub-categories-target-id' : {placeholder_text_multiple: "Select Categories"},
+        '.news-hub-search #edit-field-hub-topic' : {placeholder_text_multiple: "Select Topic(s)"},
+        '.news-hub-search #edit-type' : {placeholder_text_multiple: "Select Type(s)"},
+        '.news-hub-search #edit-field-hub-categories' : {placeholder_text_multiple: "Select Categories"},
       }
       for (var selector in config) {
         $(selector).chosen(config[selector]);
