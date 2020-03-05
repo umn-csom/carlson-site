3ae68d369bd9da34ab69d3610fa51f274852d1d9
Tim Luong <timluong@x-160-94-115-78.csom.umn.edu>
2020-03-05 14:47:45 -0600

    element class formatter 8.x-1.0-alpha1 -> 8.x-1.0

--- modules/contrib/element_class_formatter/src/Plugin/Field/FieldFormatter/TelephoneLinkClassFormatter.php
+++ modules/contrib/element_class_formatter/src/Plugin/Field/FieldFormatter/TelephoneLinkClassFormatter.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\element_class_formatter\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\telephone\Plugin\Field\FieldFormatter\TelephoneLinkFormatter;
 
 /**
@@ -19,43 +17,6 @@ use Drupal\telephone\Plugin\Field\FieldFormatter\TelephoneLinkFormatter;
  */
 class TelephoneLinkClassFormatter extends TelephoneLinkFormatter {
 
-  use ElementClassTrait;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function defaultSettings() {
-    return ElementClassTrait::elementClassDefaultSettings(parent::defaultSettings());
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsForm(array $form, FormStateInterface $form_state) {
-    $elements = parent::settingsForm($form, $form_state);
-    $class = $this->getSetting('class');
-
-    return $this->elementClassSettingsForm($elements, $class);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsSummary() {
-    $summary = parent::settingsSummary();
-    $class = $this->getSetting('class');
-
-    return $this->elementClassSettingsSummary($summary, $class);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = parent::viewElements($items, $langcode);
-    $class = $this->getSetting('class');
-
-    return $this->setElementClass($elements, $class, $items);
-  }
+  use ElementLinkClassTrait;
 
 }
