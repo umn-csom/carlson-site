4b2489fcd7387d06a772a9175a7c5f02ab2850f9
Tim Luong <luong108@umn.edu>
2020-11-30 17:05:40 -0600

    mobile styling + header code

---
+++ themes/custom/barrio_carlson/js/strategic-plan.js
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * Tabbed Content utilities.
+ *
+ */
+(function ($, Drupal) {
+    'use strict';
+
+    var position1, position2, position3, position4, position5 = 0;
+
+    function setStrategicPositions() {
+        position1 = $('#platform-section-1').offset().top;
+        position2 = $('#platform-section-2').offset().top;
+        position3 = $('#platform-section-3').offset().top;
+        position4 = $('#platform-section-4').offset().top;
+        position5 = $('#platform-section-5').offset().top;
+    }
+
+    function highlightStrategic() {
+        let currentHeight = $(window).scrollTop();
+        let position = 0;
+        if (currentHeight > position1) {
+            position = '1';
+        }
+        if (currentHeight > position2) {
+            position = '2';
+        }
+        if (currentHeight > position3) {
+            position = '3';
+        }
+        if (currentHeight > position4) {
+            position = '4';
+        }
+        if (currentHeight > position5) {
+            position = '5';
+        }
+
+        if (position > 0) {
+            $('.nav-number').removeClass('highlight');
+            $('#nav-number-' + position).addClass('highlight');
+        }
+    }
+
+    if ($('.strategic-plan').length > 0) {
+
+        $(window).resize(setStrategicPositions)
+        $(window).scroll(highlightStrategic)
+    
+        setStrategicPositions();
+        highlightStrategic();
+    }
+
+
+})(jQuery, Drupal);
