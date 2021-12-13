4b1ae0496367be7d9d1eaec8571d5c0c682de27c
Tim Luong <luong108@umn.edu>
2021-12-13 16:52:05 -0600

    comparison chart coding round 1

--- themes/custom/barrio_carlson/js/comparison-table.js
+++ themes/custom/barrio_carlson/js/comparison-table.js
@@ -5,8 +5,11 @@
  */
  (function ($, Drupal) {
     'use strict';
-    var table_array = [
-        {
+
+    var table_limit = 3;
+
+    var table_array = {
+        full_time : {
             id: "full_time",
             display: "Full Time",
             avg_gmat: 690,
@@ -15,7 +18,7 @@
             application_deadline: "Round 1: October 1, <br> Round 2: December 1, <br> Round 3: February 1"
         },
         
-        {
+        part_time : {
             id: "part_time",
             display: "Part Time",
             avg_gmat: 690,
@@ -24,7 +27,7 @@
             application_deadline: "Round 1: October 1, <br> Round 2: December 1, <br> Round 3: February 1"
         },
         
-        {
+        thing : {
             id: "thing",
             display: "Thing Time",
             avg_gmat: 690,
@@ -32,53 +35,73 @@
             gpa_avg: 3.4,
             application_deadline: "Round 1: October 1, <br> Round 2: December 1, <br> Round 3: February 1"
         }
-    ]
+    }
+
+    var render_array = ['part_time', 'full_time'];
 
     function render_table() {
-        let table = document.getElementById('example');
 
-        let rowLength = table.rows.length;
+        let render_length = render_array.length;
 
-        for(let i=0; i<rowLength; i+=1){
-            let row = table.rows[i];
+        $('#example tr').each(function(index, element) {
+            let row_element = element;
+            $(this).find('th, td').each(function(index, element) {
+                if (index > 0) {
+                    let key_thing = render_array[index - 1];
+                    if (row_element.id == 'display') {
+                        if( index <= render_length) {
+                            $( this ).find('.column-header-text').text(table_array[key_thing][row_element.id]);
+                        } else {
+                            $( this ).find('.column-header-text').text("-");
+                        }
+                    } else {
+                        if( index <= render_length) {
+                            element.innerHTML = table_array[key_thing][row_element.id];
+                        } else {
+                            element.innerHTML = "-";
+                        }
+                    }
+                }
+            });
+        })
 
-            console.log(row.id);
-
-
-            for(let j=0; j<table_array.length; j+=1) {
-                console.log(table_array[j][row.id]);
-
-                let cell = row.cells[j+1];
-
-                cell.innerHTML = table_array[j][row.id];
-            }
+        if (render_length >= table_limit) {
+            $('#comparison-menu').hide();
+        } else {
+            $('#comparison-menu').show();
         }
-
-
     }
 
     function add_table() {
-
+        if (render_array.length < table_limit) {    
+            render_array.push($('#comparison-select').val());
+        }
         render_table()
     }
 
-    function delete_table() {
-
+    function delete_table(index) {
+        if (render_array.length > 0) {
+            render_array.splice(index, 1)
+        }
         render_table()
     }
 
     $( document ).ready( function() {
         if ($('.comparison-menu__select').length > 0) {
-            table_array.forEach(element => {
-                let o = new Option(element.display, element.id);
-                $('#select').append($(o));
-            });
+            for(const [key, value] of Object.entries(table_array)) {
+                let o = new Option(value.display, value.id);
+                $('#comparison-select').append($(o));
+            }
+
         }
 
-        console.log("wow!!");
+        $('#comparison-add').on("click", add_table);
+
+        $(".column-header-button").on('click', function() {
+            delete_table($(this).data('table-index'));
+        })
+
         render_table();
     });
-
-
 
   })(jQuery, Drupal);
