d7d0e850b1f7768398b158bd1d3eb69f72e258d1
hercson <santanderhercson@gmail.com>
2021-12-13 21:42:02 -0500

    UCQ-12: modify config for line 11 (quiz questions)

--- modules/custom/carlson_quiz/src/Controller/QuizController.php
+++ modules/custom/carlson_quiz/src/Controller/QuizController.php
@@ -15,12 +15,26 @@ class QuizController extends ControllerBase {
 
     foreach ($data as $key => $pid) {
       if($key != 'results' && $pid && $pid !== '') {
-        $answer = Paragraph::load($pid);
-        if(isset($answer) && $answer->hasField('field_quiz_answer_result') && !empty($answer->get('field_quiz_answer_result'))) {
-          $answer_results = $answer->get('field_quiz_answer_result')->getValue();
-          foreach ($answer_results as $answer_result) {
-            $id = $answer_result["target_id"];
-            $results[$id] = array_key_exists($id, $results) ? ++$results[$id] : 1;
+        if(is_array($pid)) {
+          foreach ($pid as $pid_item) {
+            $answer = Paragraph::load($pid_item);
+            if(isset($answer) && $answer->hasField('field_quiz_answer_result') && !empty($answer->get('field_quiz_answer_result'))) {
+              $answer_results = $answer->get('field_quiz_answer_result')->getValue();
+              foreach ($answer_results as $answer_result) {
+                $id = $answer_result["target_id"];
+                $results[$id] = array_key_exists($id, $results) ? ++$results[$id] : 1;
+              }
+            }
+          }
+        }
+        else {
+          $answer = Paragraph::load($pid);
+          if(isset($answer) && $answer->hasField('field_quiz_answer_result') && !empty($answer->get('field_quiz_answer_result'))) {
+            $answer_results = $answer->get('field_quiz_answer_result')->getValue();
+            foreach ($answer_results as $answer_result) {
+              $id = $answer_result["target_id"];
+              $results[$id] = array_key_exists($id, $results) ? ++$results[$id] : 1;
+            }
           }
         }
       }
