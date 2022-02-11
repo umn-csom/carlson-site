fe7c835b34fbd38197fc04c12b239f683f83dda7
Tim Luong <luong108@umn.edu>
2022-02-11 17:14:58 -0600

    faculty edit menu v3

--- themes/custom/barrio_carlson/js/faculty.js
+++ themes/custom/barrio_carlson/js/faculty.js
@@ -10,4 +10,9 @@
         $('#faculty-profile__confirm').attr('open', true);
     });
 
+    $('#faculty-profile__close').on("click", function() {
+        console.log('please close');
+        $('#faculty-profile__confirm').attr('open', false);
+    })
+
 })(jQuery, Drupal);
