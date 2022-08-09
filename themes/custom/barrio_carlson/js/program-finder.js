341cf6868a49c7483c7f01513dab353397b36fb1
Tim Luong <luong108@umn.edu>
2022-08-09 14:32:32 -0500

    strip out old select2 module from themes

--- themes/custom/barrio_carlson/js/program-finder.js
+++ themes/custom/barrio_carlson/js/program-finder.js
@@ -123,14 +123,5 @@
 
     programFinder.bind();
 
-    //this drupal behavior will be run on every page ajax call
-    Drupal.behaviors.viewFiltersMultiSelect = {
-        attach: function (context, settings) {
-            $('.view-filters select[multiple]').select2({
-                placeholder: 'Select Option(s)',
-                width: '100%'
-            });
-        }
-    }
 
 })(jQuery, Drupal);
