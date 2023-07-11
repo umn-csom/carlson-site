bb8536e9b67758a4e5927965fb8a3506c269423c
James Wilson <jrwilson3@gmail.com>
2023-07-11 16:15:26 -0500

    CSM-26: Theme mobile site branding in header & mmenu flyout

--- themes/custom/carlson_refresh/js/responsive-menu-tweak.js
+++ themes/custom/carlson_refresh/js/responsive-menu-tweak.js
@@ -5,7 +5,12 @@
   Drupal.behaviors.responsiveMenuTweak = {
     attach: function () {
       var mm_menu = $(".mm-menu"),
-        branding = '<div class="mm-navbar__branding"><div class="mm-navbar__logo" /><img src="/sites/carlsonschool.umn.edu/themes/custom/carlson_refresh/images/svg/logo.svg" alt="Carlson School of Management with UMN block M branding lockup"></div><a class="mm-btn--close" id="close-nav" href="#mm-0" aria-label="close menu">&times;</a>';
+        branding = $(".site-branding__logo--mobile").clone()
+          .wrap('<div class="mm-navbar__logo" />').parent()
+          .wrap('<div class="mm-navbar__branding" />').parent()
+          .append(
+            '<a class="mm-btn--close" id="close-nav" href="#mm-0" aria-label="close menu">&times;</a>'
+          );
       if (!mm_menu.hasClass("mm-menu--branded")) {
         mm_menu.prepend(branding);
         mm_menu.addClass("mm-menu--branded");
