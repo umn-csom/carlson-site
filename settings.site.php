ab6e5e3e4fd36e54e58c53f6b0435fbd7d62bf40
James Wilson <jrwilson3@gmail.com>
2023-08-01 02:29:20 -0500

    CSM-54: Enable verbose log output on non-PROD environments

--- settings.site.php
+++ settings.site.php
@@ -39,12 +39,14 @@ switch ($environment) {
     $base_url = 'https://carlsonschool.dev.umn.edu';
     $config['environment_indicator.indicator']['bg_color'] = '#4DB9AF';
     $config['environment_indicator.indicator']['name'] = 'DEV';
+    $config['system.logging']['error_level'] = 'verbose';
     break;
 
   case 'test':
     $base_url = 'https://carlsonschool.stg.umn.edu';
     $config['environment_indicator.indicator']['bg_color'] = '#F8AF1F';
     $config['environment_indicator.indicator']['name'] = 'STAGE';
+    $config['system.logging']['error_level'] = 'verbose';
     break;
 
   case 'prod':
@@ -59,6 +61,7 @@ switch ($environment) {
     $config['environment_indicator.indicator']['bg_color'] = '#4D4D4D';
     $config['environment_indicator.indicator']['name'] = 'LOCAL';
     $config['shield.settings']['shield_enable'] = FALSE;
+    $config['system.logging']['error_level'] = 'verbose';
 }
 
 // Block robots from indexing non-prod environments.
