0a80ad7c45f0d230caea4a4b47a6e0866054cd40
James Wilson <jrwilson3@gmail.com>
2023-08-09 19:05:02 -0500

    CSM-58: Refactor sidenav mobile collapsed functionality

---
+++ themes/custom/carlson_refresh/js/sticky-menu__collapsed-trigger.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal, window) {
+  "use strict";
+
+  Drupal.behaviors.sticky_menu__collapsed_trigger = {
+    attach: function (context) {
+      let trigger = document.querySelectorAll(".sticky-menu__collapsed-trigger", context);
+      Array.prototype.forEach.call(trigger, function (el) {
+        el.addEventListener("click", function () {
+          this.setAttribute(
+            "aria-expanded",
+            this.getAttribute("aria-expanded") === "true" ? "false" : "true"
+          );
+        });
+      });
+    },
+  };
+})(jQuery, Drupal, window);
