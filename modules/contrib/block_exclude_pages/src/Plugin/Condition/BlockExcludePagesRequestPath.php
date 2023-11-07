0a0b1728edcf18cd6e8784eac761c4874edc8364
Tim Luong <luong108@umn.edu>
2023-11-07 10:37:23 -0600

    block_exclude_pages beta3 update

---
+++ modules/contrib/block_exclude_pages/src/Plugin/Condition/BlockExcludePagesRequestPath.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\block_exclude_pages\Plugin\Condition;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\system\Plugin\Condition\RequestPath;
+
+/**
+ * {@inheritdoc}
+ */
+class BlockExcludePagesRequestPath extends RequestPath {
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $paths = array_map('trim', explode("\n", $form_state->getValue('pages')));
+    foreach ($paths as $path) {
+      // Added extra condition to if statement to allow for '!/' to be used.
+      if (empty($path) || $path === '<front>' || str_starts_with($path, '/') || str_starts_with($path, '!/') ) {
+        continue;
+      }
+      $form_state->setErrorByName('pages', $this->t("The path %path requires a leading forward slash or exclamation point when used with the Pages setting.", ['%path' => $path]));
+    }
+  }
+}
