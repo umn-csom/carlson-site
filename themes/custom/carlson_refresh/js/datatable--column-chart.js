badad3d1bdfecd7cfc6c29b501b8e10a05838e45
James Wilson <jrwilson3@gmail.com>
2024-02-01 16:40:15 -0500

    CSM-157: Fix column chart theming (follow up to CSM-155 a09ebb02)

--- themes/custom/carlson_refresh/js/datatable--column-chart.js
+++ themes/custom/carlson_refresh/js/datatable--column-chart.js
@@ -1,7 +1,7 @@
 (function (Drupal, $) {
   Drupal.behaviors.verticalStackedBarChart = {
     attach: function () {
-      var table = $(".csom-column-chart");
+      var table = $(".csom-datatable--column-chart");
       table.find("tbody tr > :last-child").each(function (index) {
         var data = $(this).text();
         $(this).parents("tr").css("height", data);
