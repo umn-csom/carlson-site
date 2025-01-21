f6ad8d5722dba33942f7acab203e3072b67d5527
James Wilson <jrwilson3@gmail.com>
2025-01-21 18:39:30 -0500

    CSM-214: integrate external link icon with extlink module

--- themes/custom/carlson_refresh/js/external-link-accessibility.js
+++ themes/custom/carlson_refresh/js/external-link-accessibility.js
@@ -6,7 +6,10 @@
         .querySelectorAll('a[target="_blank"]', context)
         .forEach((link, index) => {
           // Skip if already processed.
-          if (link.querySelector("svg") || link.querySelector(".sr-only")) {
+          if (
+            link.querySelector(".external-link-icon") ||
+            link.querySelector(".external-link-text")
+          ) {
             return;
           }
           link.insertAdjacentHTML(
