c0d31b6f26abd9b346c68e7d7103889e7e1ba36e
marthinal <jm.rodriguez.velez@gmail.com>
2025-01-31 13:58:42 +0100

    CSM-209: Improve Unbounce Sticky Bar handling: Apply role="banner" to all instances

--- themes/custom/carlson_refresh/js/unbounce.js
+++ themes/custom/carlson_refresh/js/unbounce.js
@@ -5,20 +5,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Set an interval to check for the Unbounce Sticky Bar every 2 seconds
   let checkExist = setInterval(function() {
-    // Query for the sticky bar container
-    let stickyBar = document.querySelector('.ub-emb-container');
+    let stickyBars = document.querySelectorAll('.ub-emb-container');
     executionCount++;
-
-    // If the sticky bar exists, set the role attribute and stop checking
-    if (stickyBar) {
-      stickyBar.setAttribute('role', 'banner');
-      // Verify that the role attribute has been set successfully
-      if (stickyBar.getAttribute('role') === 'banner') {
-        clearInterval(checkExist);
-      }
+  
+    if (stickyBars.length) {
+      stickyBars.forEach((stickyBar) => {
+        stickyBar.setAttribute('role', 'banner');
+      });
+      clearInterval(checkExist);
     }
-
-    // Stop checking after the maximum number of executions
+  
     if (executionCount >= maxExecutions) {
       clearInterval(checkExist);
     }
