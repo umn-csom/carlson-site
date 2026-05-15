2ac7ff58481c4e5a4808491d3201d290057bd3e1
marthinal <jm.rodriguez.velez@gmail.com>
2026-05-15 10:53:19 +0200

    CSM-219: Guard responsive off-canvas route lookup

--- modules/custom/carlson_general/src/ResponsiveOffCanvasAjax.php
+++ modules/custom/carlson_general/src/ResponsiveOffCanvasAjax.php
@@ -3,6 +3,7 @@
 namespace Drupal\carlson_general;
 
 use Drupal\Core\Url;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 /**
  * Builds the asynchronous responsive off-canvas menu pieces.
@@ -53,9 +54,17 @@ class ResponsiveOffCanvasAjax {
     static::attachResponsiveMenuAssets($build);
     $build['#attached']['library'][] =
       'carlson_general/responsive_off_canvas_ajax';
+    try {
+      $endpoint = Url::fromRoute('carlson_general.responsive_off_canvas')
+        ->toString();
+    }
+    catch (RouteNotFoundException) {
+      $endpoint = Url::fromUserInput('/carlson-general/responsive-off-canvas')
+        ->toString();
+    }
+
     $build['#attached']['drupalSettings']['carlsonGeneral']['responsiveOffCanvas'] = [
-      'endpoint' => Url::fromRoute('carlson_general.responsive_off_canvas')
-        ->toString(),
+      'endpoint' => $endpoint,
       'prefetch' => FALSE,
     ];
 
